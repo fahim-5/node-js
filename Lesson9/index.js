855d8427a32c38e49043d37be1f4d77421abafbf
fahim-faysal <mfaysal223224bscse.uiu.ac.bd>
2024-09-21 22:59:46 +0600

    area calulator porject done

--- Lesson9/index.js
+++ Lesson9/index.js
@@ -1,11 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello fahim faysal</h1>
-</body>
-</html>
+const express=require("express");
+var bodyParser = require('body-parser')
+
+
+
+const app=express();
+const PORT=3000;
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// app.get("/",(req,res)=>{
+//     const id=req.query.id;
+//     const name=req.query.name;
+//     res.send(`Student name is ${name} and the id number is ${id}`);
+// })
+
+
+
+
+app.get("/register",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/register",(req,res)=>{
+    const fullname=req.body.fullname;
+    const age=req.body.age;
+    res.send(`Student name is ${fullname} and the id number is ${age}`);
+})
+
+app.listen(PORT,()=>{
+    console.log(`The server is runnig at hrrp://localhost:${PORT}`);
+})
